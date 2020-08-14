890ec97495d9c23b612249280126175a3899f99c
fengyla <fengyla@jiedaibao.com>
2020-08-14 17:27:05 +0800

    init

---
+++ src/test/java/com/kimi/leetfight/LeetfightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimi.leetfight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetfightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
