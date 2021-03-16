d89be447ec40d07f60b0778337f06bfff64261ca
Kimi <Foreverlong90@163.com>
2021-03-17 00:48:40 +0800

    0317 up

---
+++ src/main/java/com/kimi/concurrency/SemaphoreDemo.java
@@ -0,0 +1,37 @@
+package com.kimi.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created on 2021/3/17.
+ * Copyright xKimi.inc
+ *
+ * @author Kimi
+ */
+public class SemaphoreDemo implements Runnable {
+
+    final Semaphore semaphore = new Semaphore(5);
+
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getId() + ": done ！");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(20);
+        final  SemaphoreDemo demo = new SemaphoreDemo();
+        for (int i = 0; i < 20; i++) {
+            exec.submit(demo);
+        }
+    }
+}
