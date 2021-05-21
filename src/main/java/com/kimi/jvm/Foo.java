43db6b38dc5ff1b994a588231f15720f4c4bba4e
Kimi <KimiFeng@Kimi.com>
2021-05-21 18:40:42 +0800

    up 0521

---
+++ src/main/java/com/kimi/jvm/Foo.java
@@ -0,0 +1,24 @@
+package com.kimi.jvm;
+
+/**
+ * @author Kimi
+ * @date 2021/5/19 16:20
+ */
+public class Foo {
+
+    private int tryBlock;
+    private int catchBlock;
+    private int finallyBlock;
+    private int methodExit;
+
+    public void test() {
+        try {
+            tryBlock = 0;
+        } catch (Exception e) {
+            catchBlock = 1;
+        } finally {
+            finallyBlock = 2;
+        }
+        methodExit = 3;
+    }
+}
